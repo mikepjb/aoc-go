52e507743b3453966dfdbdb871832a4b23e2075f
Mike Bruce <mikepjb@proton.me>
2024-02-23 12:28:19 +0000

    day 1 part 1

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"strconv"
 	"strings"
 )
 
@@ -16,10 +17,46 @@ var inputDayOne string
 func main() {
 	fmt.Println("hello world")
 	// fmt.Println(inputDayOne)
-	line := strings.Split(inputDayOne, "\n")
-	fmt.Println(line[0])
+	lines := strings.Split(strings.Trim(inputDayOne, "\n"), "\n")
+	fmt.Println(SumLines(lines))
+}
+
+func reverse(str string) (result string) {
+	for _, v := range str {
+		result = string(v) + result
+	}
+	return result
 }
 
 func FirstNumber(line string) string {
-	return "4"
+	pos := strings.IndexAny(line, "123456789")
+	if pos != -1 {
+		return string(line[pos])
+	}
+	return "NOTFOUND"
+}
+
+func LastNumber(line string) string {
+	rl := reverse(line)
+	pos := strings.IndexAny(rl, "123456789")
+	if pos != -1 {
+		return string(rl[pos])
+	}
+	return "NOTFOUND"
+}
+
+func FirstLast(line string) string {
+	return FirstNumber(line) + LastNumber(line)
+}
+
+func SumLines(lines []string) int {
+	total := 0
+	for _, l := range lines {
+		n, err := strconv.Atoi(FirstLast(l))
+		if err != nil {
+			fmt.Printf("Could not convert %v to number\n", l)
+		}
+		total += n
+	}
+	return total
 }
--- cmd/main_test.go
+++ cmd/main_test.go
@@ -1,10 +1,46 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestGetFirstNumber(t *testing.T) {
-	line := "4fosdijfdsa7"
-	if FirstNumber(line) != "4" {
-		t.Fatalf("that is wrong")
+	line := "9fo4sd9ij2fdsa7"
+	expectation := "9"
+	result := FirstNumber(line)
+	if result != expectation {
+		t.Fatalf("that is wrong, expected %s, got %s", expectation, result)
+	}
+}
+
+func TestGetLastNumber(t *testing.T) {
+	line := "9fo4sd9ij2fdsa7"
+	expectation := "7"
+	result := LastNumber(line)
+	if result != expectation {
+		t.Fatalf("that is wrong, expected %s, got %s", expectation, result)
+	}
+}
+
+func TestFirstLast(t *testing.T) {
+	line := "9fo4sd9ij2fdsa7"
+	expectation := "97"
+	result := FirstLast(line)
+	if result != expectation {
+		t.Fatalf("that is wrong, expected %s, got %s", expectation, result)
+	}
+}
+
+func TestSumLines(t *testing.T) {
+	lines := []string{
+		"1abc2",
+		"pqr3stu8vwx",
+		"a1b2c3d4e5f",
+		"treb7uchet",
+	}
+	expectation := 142
+	result := SumLines(lines)
+	if result != expectation {
+		t.Fatalf("that is wrong, expected %v, got %v", expectation, result)
 	}
 }
